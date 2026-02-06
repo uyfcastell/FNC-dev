2d25c4bf446e3934330e1ba2afedf0c97426e9b4
Fernando Castellano <uyfcastell@gmail.com>
2026-02-06 15:36:34 -0300

    Fix down_revision for orders.submit Alembic migration

--- backend/alembic/versions/20260206_0021_add_orders_submit_permission.py
+++ backend/alembic/versions/20260206_0021_add_orders_submit_permission.py
@@ -11,7 +11,7 @@ from alembic import op
 
 
 revision: str = "20260206_0021"
-down_revision: Union[str, None] = "20250922_0020_overhead_items_piece_rates"
+down_revision: Union[str, None] = "20250922_0020"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
